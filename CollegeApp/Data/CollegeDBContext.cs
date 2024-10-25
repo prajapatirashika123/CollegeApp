0ac6286508ead29cbb1864bb39040e9c790fc618
Shanu <shanu12pr@gmail.com>
2024-10-26 01:18:20 +0530

    Part 39 : Add db context
    Need to check log part

---
+++ CollegeApp/Data/CollegeDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CollegeApp.Data
+{
+    public class CollegeDBContext : DbContext
+    {
+        public CollegeDBContext(DbContextOptions<CollegeDBContext> options) : base(options)
+        {
+        }
+
+        private DbSet<Student> Students { get; set; }
+    }
+}
+}
--- CollegeApp/Program.cs
+++ CollegeApp/Program.cs
@@ -1,9 +1,14 @@
+using CollegeApp.Data;
 using CollegeApp.MyLogging;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<CollegeDBContext>(options =>
+{
+    options.UseSqlServer("Data Source=(local);Initial Catalog=CollegeAppDB;Integrated Security=True;Trust Server Certificate=True");
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
