df63f51b15c4c8b877e1ad4ae68b53316fcce09b
Shanu <shanu12pr@gmail.com>
2024-10-25 22:43:41 +0530

    Part-15 : Model  Validations

--- CollegeApp/Models/StudentDTO.cs
+++ CollegeApp/Models/StudentDTO.cs
@@ -1,10 +1,18 @@
-﻿namespace CollegeApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CollegeApp.Models
 {
     public class StudentDTO
     {
         public int Id { get; set; }
+
+        [Required]
         public string StudentName { get; set; }
+
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required]
         public string Address { get; set; }
     }
 }
