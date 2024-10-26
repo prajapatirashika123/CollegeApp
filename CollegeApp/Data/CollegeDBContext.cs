fc46419b9c0fdbe3f30e0ebd4a3885ac17ae48f4
Shanu <shanu12pr@gmail.com>
2024-10-26 15:58:30 +0530

    Part-41 : add data to tables in ef core code first

--- CollegeApp/Data/CollegeDBContext.cs
+++ CollegeApp/Data/CollegeDBContext.cs
@@ -9,5 +9,27 @@ namespace CollegeApp.Data
         }
 
         private DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>().HasData(new List<Student>()
+            {
+                new Student {
+                    Id =1,
+                    StudentName="AVc",
+                    Email="aih@gmail.com",
+                    DOB = new DateTime(2022,12,3),
+                    Address="123,4,qasjh"
+                },
+                new Student {
+                    Id =2,
+                    StudentName="Iyt",
+                    Email="iyt@gmail.com",
+                    DOB = new DateTime(2022,8,23),
+                    Address="wnx.s8,we"
+                },
+            });
+            // base.OnModelCreating(modelBuilder);
+        }
     }
 }
---
+++ CollegeApp/Migrations/20241026102323_AddDataToStudentsTable.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CollegeApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDataToStudentsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "Id", "Address", "DOB", "Email", "StudentName" },
+                values: new object[,]
+                {
+                    { 1, "123,4,qasjh", new DateTime(2022, 12, 3, 0, 0, 0, 0, DateTimeKind.Unspecified), "aih@gmail.com", "AVc" },
+                    { 2, "wnx.s8,we", new DateTime(2022, 8, 23, 0, 0, 0, 0, DateTimeKind.Unspecified), "iyt@gmail.com", "Iyt" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
